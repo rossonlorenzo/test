829e7f4410891a4c346908d0b7e45a8cdaf0fa9c
Ciro9320 <ciropassata93@gmail.com>
2023-11-28 18:36:48 +0100

    test ruff

--- src/sample.py
+++ src/sample.py
@@ -1,6 +1,8 @@
 import os
 
+
 def prints():
     print("Sample Python Programm...Sample Python Programm...Sample Python Programm...Sample Python Programm...Sample Python Programm...Sample Python Programm...Sample Python Programm...Sample Python Programm...")
+
 
 prints()
